91ba040b1df16b5c55afa97d04382589e0feddbf
abhineetyellisheti-commits <abhineet.yellisheti@gmail.com>
2026-09-18 16:36:35 +0530

    Update problem02.c

--- problem02.c
+++ problem02.c
@@ -1,2 +1,34 @@
 //2. Write a C program to add two numbers.
 
+#include <stdio.h>
+
+
+void input(int *a, int *b)
+{
+    printf("Enter two numbers: ");
+    scanf("%d %d", a, b);
+}
+
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+
+void display(int result)
+{
+    printf("Sum = %d\n", result);
+}
+
+
+int main()
+{
+    int a, b, result;
+
+    input(&a, &b);
+    result = add(a, b);
+    display(result);
+
+    return 0;
+}
